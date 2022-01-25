0fff9e49da7ef9bc9f4a2979111e85dff923d113
root <74122080+rootslashbin@users.noreply.github.com>
2022-01-24 23:24:27 -0800

    fix

--- scripts/ipfs_rename.sh
+++ scripts/ipfs_rename.sh
@@ -5,7 +5,7 @@ cd ..
 for number in {1..40}
 do
 URL="https://raw.githubusercontent.com/rootslashbin/ea/main/"
-COMPOSED_URL="$URL$number.json"
+COMPOSED_URL="$URL$number.png"
 echo $(jq --arg variable "$COMPOSED_URL" '.image = $variable' $number.json) > $number.json
 echo "Replaced IMG for ape: $number"
 done
